04fa9b0c7afd6dc0dac430719e52d8cd74cdf880
yokefellow <yuan1241900094@gmail.com>
2020-07-06 17:11:31 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run build
+# npm run build
 
 # 进入生成的文件夹
 # cd dist
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:yokefellow/yokefellow.github.io.git master
+git push -f git@github.com:yokefellow/yokefellow.github.io.git gh-pages-source
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
