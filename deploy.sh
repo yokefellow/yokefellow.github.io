1a46a48fe0e6386475741cbeb44e8abb17d268b5
yokefellow <yuan1241900094@gmail.com>
2020-07-06 18:44:45 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,10 @@ git init
 git add -A
 git commit -m 'deploy'
 
+
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:yokefellow/yokefellow.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yokefellow/yokefellow.github.io.git master:gh-pages
+# 在GitHub创建Repo时一定要勾上生成README.md文件的选项，否则执行下面这句话后没有master分支
+git push -f git@github.com:yokefellow/yokefellow.github.io.git master:gh-pages-source
